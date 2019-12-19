b6d885dc8514f702fa2787c8ce4485938e3879b3
Pistis <danielpartey.dp@gmail.com>
2019-12-19 13:20:17 +0000

    [IMP] added a dummy splash screen

---
+++ app/src/main/java/com/deemiensa/dewormingmanager/SplashScreen.kt
@@ -0,0 +1,41 @@
+package com.deemiensa.dewormingmanager
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+
+    private var mDelayHandler: Handler? = null
+    private val mSplashDelay: Long = 3000 //3 seconds
+
+    private val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        //Initialize the Handler
+        mDelayHandler = Handler()
+
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, mSplashDelay)
+    }
+
+    public override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+}
